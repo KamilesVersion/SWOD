299900943773777f63a873f83cf247a26c2a2f02
crybbyboy <jiglipuf4life@gmail.com>
2025-02-22 15:14:29 +0200

    pradeta sukurti duomenu baze

--- SWOD/SWOD/swod.py
+++ SWOD/SWOD/swod.py
@@ -1,27 +1,23 @@
-# from flask import Flask
-
-# # Create an instance of the Flask class that is the WSGI application.
-# # The first argument is the name of the application module or package,
-# # typically __name__ when using a single module.
-# app = Flask(__name__)
-
-# # Flask route decorators map / and /hello to the hello function.
-# # To add other resources, create functions that generate the page contents
-# # and add decorators to define the appropriate resource locators for them.
-
-# @app.route('/')
-# @app.route('/hello')
-# def hello():
-#    # Render the page
-#    return "Pakeista labas"
-
-# if __name__ == '__main__':
-#    # Run the app server on localhost:4449
-#    app.run('localhost', 4449)
 
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
 
 app = Flask(__name__)
+
+#bcrypt = Bcrypt(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+
+db = SQLAlchemy(app)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+
 
 @app.route('/')
 def home():
