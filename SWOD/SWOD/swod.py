2e497f1919e45d1a5bc21da79fe72c1f2e45d3ac
Ieva Šalašavičiūtė <ievsal4@ktu.lt>
2025-02-22 13:52:19 +0200

    sutvarkiau buga

--- SWOD/SWOD/swod.py
+++ SWOD/SWOD/swod.py
@@ -26,12 +26,14 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
+
+    
 @app.route('/login')
 def login():
     return render_template('login.html')
 
 @app.route('/register')
-def login():
+def register():
     return render_template('register.html')
 
 if(__name__) == '__main__':
