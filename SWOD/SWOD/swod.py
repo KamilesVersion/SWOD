7495ba996953363f013a159035e3537f5265000a
Kamile's Version <kamilevaskyte04@gmail.com>
2025-04-12 19:29:30 +0300

    Fixed profile link

--- SWOD/SWOD/swod.py
+++ SWOD/SWOD/swod.py
@@ -1275,9 +1275,9 @@ def artist_top_tracks():
 
 #----------------------------------TEST---------------------------------------------------
 
-# @app.route('/index')
-# def index():
-#     return render_template('index.html')
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 
 
